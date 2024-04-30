8b21a28daee92145ce5d00fc62611071fe046f0f
Ravejaja <xukeita@proton.me>
2024-04-30 16:48:37 +0100

    Login Page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,43 +2,100 @@ import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
-
+  static const TextStyle blackLabelStyle = TextStyle(color: Colors.black);
   final backgroundColor = const Color(0xFF672D6F);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  label: Text('E-mail'),
-                  hintText: 'exemplo@gmail.com',
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        inputDecorationTheme: const InputDecorationTheme(
+          labelStyle: blackLabelStyle, // Hint text color
+        ),
+      ),
+      home: Scaffold(
+        body: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Image.asset(
+                  "assets/images/user.png",
+                  width: 150,
+                  height: 150,
                 ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  label: Text('Palavra-Passe'),
-                  hintText: 'Digite sua Palavra-Passe',
+                const SizedBox(height: 35),
+                TextFormField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    prefixIcon: const Icon(Icons.email),
+                    label: const Text('E-mail'),
+                    hintText: 'exemplo@gmail.com',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(10),                
+                    ),
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: backgroundColor,
-                  foregroundColor: Colors.white,
-
+                const SizedBox(height: 15),
+                TextFormField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    prefixIcon: const Icon(Icons.lock),
+                    label: const Text('Palavra-Passe'),
+                    hintText: 'Digite sua Palavra-Passe',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
                 ),
-                child: Text('Acessar'),
-              ),
-            ],
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: const Text(
+                    'Esqueceu a Palavra-passe?',
+                    style: TextStyle(decoration: TextDecoration.underline),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                        const EdgeInsets.symmetric(
+                            vertical: 15, horizontal: 25)),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color(0xff672D6F)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                    ),
+                  ),
+                  child: const Text('Acessar', style: TextStyle(fontSize: 18),),
+                ),
+                const SizedBox(height: 8,),
+                Container(
+                  alignment: Alignment.center,
+                  child: GestureDetector(
+                    onTap: (){
+                      Navigator.of(context).pushReplacementNamed('/register');
+                    },
+                    child: const Text(
+                      'Ainda não tem uma conta? Criar conta',
+                      
+                      style: TextStyle(decoration: TextDecoration.underline),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
