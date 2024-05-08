68e65e22294bf92fa91e088a83675a2afea59b86
ZherriDev <zherri.dev@gmail.com>
2024-05-08 11:16:38 +0100

    homepage and organization

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:byme_flutter_app/utils/fetch_user_data.dart';
+import 'package:byme_flutter_app/utils/user/fetch_user_data.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  final PageController pageController;
+
+  const HomePage({Key? key, required this.pageController}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -28,7 +30,7 @@ class _HomePageState extends State<HomePage> {
               Container(
                 padding: EdgeInsets.all(10),
                 width: MediaQuery.of(context).size.width / 2 + 160,
-                height: 200,
+                height: 230,
                 decoration: BoxDecoration(
                     color: Color(0xFF787878).withOpacity(0.16),
                     borderRadius: BorderRadius.circular(20)),
@@ -44,16 +46,90 @@ class _HomePageState extends State<HomePage> {
                       child: Text(
                         snapshot.data?['doctor']['fullname'],
                         textAlign: TextAlign.center,
-                        style: TextStyle(color: Colors.white, fontSize: 16),
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
                       ),
                     ),
                     Container(
                       width: MediaQuery.of(context).size.width,
                       padding: EdgeInsets.all(10),
-                      child: Text(
-                        snapshot.data?['doctor']['fullname'],
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      child: Row(children: [
+                        Icon(Icons.medical_information_rounded),
+                        Container(
+                          width: 10,
+                        ),
+                        Text(
+                          snapshot.data?['doctor']['speciality'],
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ]),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.all(10),
+                      child: Row(children: [
+                        Icon(Icons.business_rounded),
+                        Container(
+                          width: 10,
+                        ),
+                        Text(
+                          snapshot.data?['doctor']['hospital'],
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ]),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.all(10),
+                      child: Row(children: [
+                        Container(
+                          width: 5,
+                        ),
+                        Text('3',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold)),
+                        Container(
+                          width: 20,
+                        ),
+                        Text(
+                          'Consultas para hoje',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ]),
+                    ),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Container(
+                        width: 30,
+                        height: 30,
+                        decoration: BoxDecoration(
+                            color: Color(0xff672D6F),
+                            borderRadius: BorderRadius.circular(100)),
+                        child: Center(
+                          child: IconButton(
+                            onPressed: () {
+                              widget.pageController.jumpToPage(3);
+                            },
+                            iconSize: 15,
+                            icon: Icon(
+                              Icons.edit,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
                       ),
                     )
                   ],
