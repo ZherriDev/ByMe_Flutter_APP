c09924fe14b4d3b532012b7f42fa0135a9ceb06a
Ravejaja <xukeita@proton.me>
2024-05-08 11:16:48 +0100

    Profile Page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,5 +1,7 @@
-
+import 'package:byme_flutter_app/utils/fetch_user_data.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class DoctorProfile extends StatefulWidget {
   const DoctorProfile({super.key});
@@ -11,6 +13,86 @@ class DoctorProfile extends StatefulWidget {
 class _DoctorProfileState extends State<DoctorProfile> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Column(children: [Text('')],),),);
+    return FutureBuilder<Map<String, dynamic>?>(
+        future: fetchUserData(context),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError || snapshot.data == null) {
+            return Center(
+              child: Text('Erro ao carregar dados'),
+            );
+          } else {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      snapshot.data?['doctor']['fullname'],
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.business_rounded),
+                        Text(snapshot.data?['doctor']['hospital']),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Color(0xFF672D6F),
+                          borderRadius: BorderRadius.circular(12)),
+                      width: 80,
+                      height: 80,
+                      
+                      child: Column(
+                        children: [
+                          Text(
+                            'Consultas Agendadas',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Text(
+                            snapshot.data?['doctor'][''],
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      
+                    ),
+                    SizedBox(width: 60),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Color(0xFF672D6F),
+                          borderRadius: BorderRadius.circular(12)),
+                      width: 80,
+                      height: 80,
+                      child: Text(
+                        'Pacientes',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            );
+          }
+        });
   }
 }
