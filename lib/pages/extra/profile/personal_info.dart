564abc493ef90826dbbc2ff9bb5a7deb275136f1
Ravejaja <xukeita@proton.me>
2024-05-10 12:45:07 +0100

    Personal Info page

--- lib/pages/extra/profile/personal_info.dart
+++ lib/pages/extra/profile/personal_info.dart
@@ -1,5 +1,6 @@
 import 'package:byme_flutter_app/utils/user/fetch_user_data.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class PersonalInfo extends StatefulWidget {
   final PageController pageController;
@@ -22,6 +23,19 @@ class _PersonalInfoState extends State<PersonalInfo> {
     return todayDate;
   }
 
+  final List<String> _specialties = [
+    'Clínico Geral',
+    'Cardiologista',
+    'Dermatologista',
+    'Endocrinologista',
+    'Gastroenterologista',
+    'Ginecologista',
+    'Neurologista',
+    'Ortopedista',
+    'Pediatra',
+    'Psiquiatra',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<Map<String, dynamic>?>(
@@ -36,11 +50,155 @@ class _PersonalInfoState extends State<PersonalInfo> {
               child: Text('Erro ao carregar dados'),
             );
           } else {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [Text('Informações Pessoais')],
+            final name = TextEditingController(
+              text: snapshot.data?['user']['doctor']['fullname'] ?? ' ',
+            );
+            final phone = TextEditingController(
+              text: snapshot.data?['user']['doctor']['telephone'].toString() ??
+                  '',
+            );
+
+            final _birthdate = TextEditingController(
+                text:
+                    snapshot.data?['user']['doctor']['birthdate'].toString() ??
+                        '');
+            print(_birthdate);
+            String speciality =
+                snapshot.data?['user']['doctor']['speciality'] ?? ' ';
+            final address = TextEditingController(
+              text: snapshot.data?['user']['doctor']['address'] ?? ' ',
+            );
+
+            return SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Informações Pessoais',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(
+                        top: 10, bottom: 10, left: 20, right: 20),
+                    height: 450,
+                    width: 400,
+                    child: Form(
+                        child: Column(
+                      children: [
+                        TextFormField(
+                          validator: (name) {
+                            if (name == null) {
+                              return 'Insira seu nome';
+                            }
+                            return null;
+                          },
+                          controller: name,
+                          decoration: InputDecoration(
+                              labelText: 'Nome Completo',
+                              filled: true,
+                              fillColor: Colors.grey[300],
+                              labelStyle: TextStyle(fontSize: 13),
+                              prefixIcon: Icon(Icons.person),
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(20))),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        TextFormField(
+                          validator: (phone) {
+                            if (phone == null) {
+                              return 'Insira seu número de telemovel';
+                            }
+                            return null;
+                          },
+                          controller: phone,
+                          decoration: InputDecoration(
+                              labelText: 'Número de Telemovel',
+                              filled: true,
+                              fillColor: Colors.grey[300],
+                              labelStyle: TextStyle(fontSize: 13),
+                              prefixIcon: Icon(Icons.person),
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(20))),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        TextFormField(
+                          readOnly: true,
+                          onTap: () {
+                            _selectDate(_birthdate);
+                          },
+                          controller: _birthdate,
+                          decoration: InputDecoration(
+                              labelText: 'Data de Nascimento',
+                              filled: true,
+                              fillColor: Colors.grey[300],
+                              labelStyle: TextStyle(fontSize: 13),
+                              prefixIcon: Icon(Icons.person),
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(20))),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        DropdownButtonFormField<String>(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.grey[200],
+                            prefixIcon: const Icon(Icons.badge_rounded),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          value: speciality,
+                          items: _specialties.map((String specialty) {
+                            return DropdownMenuItem<String>(
+                              value: specialty,
+                              child: Text(specialty),
+                            );
+                          }).toList(),
+                          onChanged: (newValue) {
+                            setState(() {
+                              speciality = newValue!;
+                            });
+                          },
+                          validator: (speciality) {
+                            if (speciality == null) {
+                              return 'Por favor, selecione uma especialidade';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    )),
+                  )
+                ],
+              ),
             );
           }
         });
+  }
+
+  Future<void> _selectDate(_birthdate) async {
+    DateTime? _picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(1950),
+        lastDate: DateTime(2025));
+
+    if (_picked != null) {
+      setState(() {
+        _birthdate.text = _picked.toString().split(" ")[0];
+      });
+    }
   }
 }
