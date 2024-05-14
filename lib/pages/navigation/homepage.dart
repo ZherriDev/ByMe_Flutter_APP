c137eee810d377b9580e35abeb27e32bd6b29834
ZherriDev <zherri.dev@gmail.com>
2024-05-14 10:40:36 +0100

    patients page finished

--- lib/pages/navigation/homepage.dart
+++ lib/pages/navigation/homepage.dart
@@ -39,206 +39,208 @@ class _HomePageState extends State<HomePage> {
               snapshot.data?['appointments']['appointments'];
           final appointmentsToday = appointments.length;
 
-          return Column(
-            children: [
-              Container(
-                padding: EdgeInsets.all(10),
-                width: MediaQuery.of(context).size.width - 40,
-                height: 230,
-                decoration: BoxDecoration(
-                    color: Color(0xFF787878).withOpacity(0.16),
-                    borderRadius: BorderRadius.circular(20)),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                          color: Color(0xff672D6F),
-                          borderRadius: BorderRadius.circular(12)),
-                      child: Text(
-                        snapshot.data?['user']['doctor']['fullname'],
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      padding: EdgeInsets.all(10),
-                      child: Row(children: [
-                        Icon(Icons.medical_information_rounded),
-                        Container(
-                          width: 10,
-                        ),
-                        Text(
-                          snapshot.data?['user']['doctor']['speciality'],
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ]),
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      padding: EdgeInsets.all(10),
-                      child: Row(children: [
-                        Icon(Icons.business_rounded),
-                        Container(
-                          width: 10,
-                        ),
-                        Text(
-                          snapshot.data?['user']['doctor']['hospital'],
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ]),
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      padding: EdgeInsets.all(10),
-                      child: Row(children: [
-                        Container(
-                          width: 5,
-                        ),
-                        Text('$appointmentsToday',
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold)),
-                        Container(
-                          width: 20,
-                        ),
-                        Text(
-                          appointmentsToday > 1
-                              ? 'Consultas para hoje'
-                              : 'Consulta para hoje',
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ]),
-                    ),
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: Container(
-                        width: 30,
-                        height: 30,
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10),
+                  width: MediaQuery.of(context).size.width - 40,
+                  height: 230,
+                  decoration: BoxDecoration(
+                      color: Color(0xFF787878).withOpacity(0.16),
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        padding: EdgeInsets.all(10),
                         decoration: BoxDecoration(
                             color: Color(0xff672D6F),
-                            borderRadius: BorderRadius.circular(100)),
-                        child: Center(
-                          child: IconButton(
-                            onPressed: () {
-                              widget.pageController.jumpToPage(3);
-                            },
-                            iconSize: 15,
-                            icon: Icon(
-                              Icons.edit,
+                            borderRadius: BorderRadius.circular(12)),
+                        child: Text(
+                          snapshot.data?['user']['doctor']['fullname'],
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
                               color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        padding: EdgeInsets.all(10),
+                        child: Row(children: [
+                          Icon(Icons.medical_information_rounded),
+                          Container(
+                            width: 10,
+                          ),
+                          Text(
+                            snapshot.data?['user']['doctor']['speciality'],
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ]),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        padding: EdgeInsets.all(10),
+                        child: Row(children: [
+                          Icon(Icons.business_rounded),
+                          Container(
+                            width: 10,
+                          ),
+                          Text(
+                            snapshot.data?['user']['doctor']['hospital'],
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ]),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        padding: EdgeInsets.all(10),
+                        child: Row(children: [
+                          Container(
+                            width: 5,
+                          ),
+                          Text('$appointmentsToday',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold)),
+                          Container(
+                            width: 20,
+                          ),
+                          Text(
+                            appointmentsToday > 1
+                                ? 'Consultas para hoje'
+                                : 'Consulta para hoje',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ]),
+                      ),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Container(
+                          width: 30,
+                          height: 30,
+                          decoration: BoxDecoration(
+                              color: Color(0xff672D6F),
+                              borderRadius: BorderRadius.circular(100)),
+                          child: Center(
+                            child: IconButton(
+                              onPressed: () {
+                                widget.pageController.jumpToPage(3);
+                              },
+                              iconSize: 15,
+                              icon: Icon(
+                                Icons.edit,
+                                color: Colors.white,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    )
-                  ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                height: 20,
-              ),
-              Text(
-                appointmentsToday > 1
-                    ? 'Detalhes das consultas:'
-                    : 'Detalhes da consulta:',
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              if (appointmentsToday >= 1)
                 Container(
-                  width: MediaQuery.of(context).size.width - 40,
-                  height: MediaQuery.of(context).size.height - 565,
-                  child: ListView.builder(
-                      itemCount: appointments.length,
-                      itemBuilder: (context, index) {
-                        String name =
-                            appointments[index]['patient_data']['name'];
-                        String processnumber = appointments[index]
-                            ['patient_data']['processnumber'];
-                        String time = appointments[index]['time'];
-
-                        return Container(
-                          width: MediaQuery.of(context).size.width,
-                          margin: EdgeInsets.symmetric(vertical: 10),
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            color: Colors.grey[200],
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                children: [
-                                  Text(
-                                    'Nome: ',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 16),
-                                  ),
-                                  Text(
-                                    '$name',
-                                    style: TextStyle(fontSize: 16),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Text(
-                                    'Nº Processo: ',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 16),
-                                  ),
-                                  Text(
-                                    '$processnumber',
-                                    style: TextStyle(fontSize: 16),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Text(
-                                    'Horário: ',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 16),
-                                  ),
-                                  Text(
-                                    '$time',
-                                    style: TextStyle(fontSize: 16),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        );
-                      }),
-                )
-              else
-                Container(
+                  height: 20,
+                ),
+                Text(
+                  appointmentsToday > 1
+                      ? 'Detalhes das consultas:'
+                      : 'Detalhes da consulta:',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                if (appointmentsToday >= 1)
+                  Container(
                     width: MediaQuery.of(context).size.width - 40,
                     height: MediaQuery.of(context).size.height - 565,
-                    child: Center(
-                      child: Text('Você não tem consultas marcadas para hoje'),
-                    ))
-            ],
+                    child: ListView.builder(
+                        itemCount: appointments.length,
+                        itemBuilder: (context, index) {
+                          String name =
+                              appointments[index]['patient_data']['name'];
+                          String processnumber = appointments[index]
+                              ['patient_data']['processnumber'];
+                          String time = appointments[index]['time'];
+            
+                          return Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: EdgeInsets.symmetric(vertical: 10),
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      'Nome: ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16),
+                                    ),
+                                    Text(
+                                      '$name',
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      'Nº Processo: ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16),
+                                    ),
+                                    Text(
+                                      '$processnumber',
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      'Horário: ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16),
+                                    ),
+                                    Text(
+                                      '$time',
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          );
+                        }),
+                  )
+                else
+                  Container(
+                      width: MediaQuery.of(context).size.width - 40,
+                      height: MediaQuery.of(context).size.height - 565,
+                      child: Center(
+                        child: Text('Você não tem consultas marcadas para hoje'),
+                      ))
+              ],
+            ),
           );
         }
       },
