68341e168179f9111e0ad59bbdc8418cc4c400dd
Ravejaja <xukeita@proton.me>
2024-05-24 13:03:28 +0100

    Formatting chaneg_image

--- lib/utils/widgets/change_image.dart
+++ lib/utils/widgets/change_image.dart
@@ -11,7 +11,7 @@ class ChangeImage extends StatefulWidget {
 }
 
 class _ChangeImageState extends State<ChangeImage> {
-  bool _isLoadingImage = false;
+  bool _isLoadingImage = false; w
 
   @override
   Widget build(BuildContext context) {
